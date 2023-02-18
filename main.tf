5bac357923b9a39aeea436266e065a11e4f5618e
Edilson Rodrigues <edilson.w3g@gmail.com>
2023-02-17 23:00:39 -0300

    feat: auto scaling

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ provider "aws" {
 }
 
 resource "aws_launch_configuration" "example" {
+  name_prefix = "terraform-lc-example"
   image_id        = "ami-0c55b159cbfafe1f0"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
@@ -12,6 +13,12 @@ resource "aws_launch_configuration" "example" {
     echo "Hello, World" > index.html
     nohup busybox httpd -f -p ${var.server_port} &
     EOF
+  
+  # Required when using a launch configuration with an auto scaling group
+  # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_security_group" "instance" {
@@ -27,6 +34,7 @@ resource "aws_security_group" "instance" {
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
   min_size             = 2
   max_size             = 10
 
@@ -41,4 +49,12 @@ variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
   default     = 8080
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
 }
